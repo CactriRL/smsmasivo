f8baeaad24d0382968549e76e6ddc3bcd0394425
CACTRI R.L <109222990+CactriRL@users.noreply.github.com>
2023-11-11 13:27:14 -0400

    NUEVO

---
+++ sms.js
@@ -0,0 +1,108 @@
+
+//Numero 1
+setTimeout(function () {
+  document.querySelector("body > mw-app > mw-bootstrap > div > main > mw-main-container > div > mw-main-nav > div > mw-fab-link > a").click();
+  setTimeout(function () {
+    var inputElement = document.querySelector("body > mw-app > mw-bootstrap > div > main > mw-main-container > div > mw-new-conversation-container > mw-new-conversation-sub-header > div > div.input-container > mw-contact-chips-input > div > div > input");if (inputElement) {
+
+      inputElement.value = "TuNúmeroDeTeléfono";
+
+      var event = new Event('input', {bubbles: true,cancelable: true,});inputElement.dispatchEvent(event);} else {
+      console.error("No se encontró el elemento input con el selector proporcionado.");}
+    setTimeout(function () {document.querySelector("body > mw-app > mw-bootstrap > div > main > mw-main-container > div > mw-new-conversation-container > div > mw-contact-selector-button > button > span.mat-mdc-button-touch-target").click();
+      setTimeout(function () {var containerElement = document.querySelector("body > mw-app > mw-bootstrap > div > main > mw-main-container > div > mw-conversation-container > div > div[class*=ng-tns]");
+        if (containerElement) {var selector = "div.container." + containerElement.classList[1] + " > div > mws-message-compose > div > div.input-box > div > div > mws-autosize-textarea > textarea";
+          var newTextareaElement = document.querySelector(selector);
+          if (newTextareaElement) {newTextareaElement.value = "CTR-ASFI Te invitan a participar en la Encuesta Nacional de Servicios Financieros 2023 del 13/Nov al 12/Dic has clic en: https://encuesta2023.asfi.gob.bo. (Tu respuesta es confidencial)";
+            var newEvent = new Event('input', {bubbles: true,cancelable: true,});newTextareaElement.dispatchEvent(newEvent);setTimeout(function () {
+              newTextareaElement.dispatchEvent(new KeyboardEvent('keydown', { key: 'Enter',bubbles: true,cancelable: true,}));}, 5000);} else {console.error("No se encontró el nuevo elemento textarea con el selector construido."); }} else {console.error("No se encontró el elemento contenedor con la clase dinámica.");}
+      }, 5000);}, 5000);}, 10000); 
+}, 5000); 
+
+
+
+//Numero 2
+setTimeout(function () {
+  document.querySelector("body > mw-app > mw-bootstrap > div > main > mw-main-container > div > mw-main-nav > div > mw-fab-link > a").click();
+  setTimeout(function () {
+    var inputElement = document.querySelector("body > mw-app > mw-bootstrap > div > main > mw-main-container > div > mw-new-conversation-container > mw-new-conversation-sub-header > div > div.input-container > mw-contact-chips-input > div > div > input");if (inputElement) {
+
+      inputElement.value = "TuNúmeroDeTeléfono";
+
+      var event = new Event('input', {bubbles: true,cancelable: true,});inputElement.dispatchEvent(event);} else {
+      console.error("No se encontró el elemento input con el selector proporcionado.");}
+    setTimeout(function () {document.querySelector("body > mw-app > mw-bootstrap > div > main > mw-main-container > div > mw-new-conversation-container > div > mw-contact-selector-button > button > span.mat-mdc-button-touch-target").click();
+      setTimeout(function () {var containerElement = document.querySelector("body > mw-app > mw-bootstrap > div > main > mw-main-container > div > mw-conversation-container > div > div[class*=ng-tns]");
+        if (containerElement) {var selector = "div.container." + containerElement.classList[1] + " > div > mws-message-compose > div > div.input-box > div > div > mws-autosize-textarea > textarea";
+          var newTextareaElement = document.querySelector(selector);
+          if (newTextareaElement) {newTextareaElement.value = "CTR-ASFI Te invitan a participar en la Encuesta Nacional de Servicios Financieros 2023 del 13/Nov al 12/Dic has clic en: https://encuesta2023.asfi.gob.bo. (Tu respuesta es confidencial)";
+            var newEvent = new Event('input', {bubbles: true,cancelable: true,});newTextareaElement.dispatchEvent(newEvent);setTimeout(function () {
+              newTextareaElement.dispatchEvent(new KeyboardEvent('keydown', { key: 'Enter',bubbles: true,cancelable: true,}));}, 5000);} else {console.error("No se encontró el nuevo elemento textarea con el selector construido."); }} else {console.error("No se encontró el elemento contenedor con la clase dinámica.");}
+      }, 5000);}, 5000);}, 10000); 
+}, 30000); 
+
+
+
+//Numero 3
+setTimeout(function () {
+  document.querySelector("body > mw-app > mw-bootstrap > div > main > mw-main-container > div > mw-main-nav > div > mw-fab-link > a").click();
+  setTimeout(function () {
+    var inputElement = document.querySelector("body > mw-app > mw-bootstrap > div > main > mw-main-container > div > mw-new-conversation-container > mw-new-conversation-sub-header > div > div.input-container > mw-contact-chips-input > div > div > input");if (inputElement) {
+
+      inputElement.value = "TuNúmeroDeTeléfono";
+
+      var event = new Event('input', {bubbles: true,cancelable: true,});inputElement.dispatchEvent(event);} else {
+      console.error("No se encontró el elemento input con el selector proporcionado.");}
+    setTimeout(function () {document.querySelector("body > mw-app > mw-bootstrap > div > main > mw-main-container > div > mw-new-conversation-container > div > mw-contact-selector-button > button > span.mat-mdc-button-touch-target").click();
+      setTimeout(function () {var containerElement = document.querySelector("body > mw-app > mw-bootstrap > div > main > mw-main-container > div > mw-conversation-container > div > div[class*=ng-tns]");
+        if (containerElement) {var selector = "div.container." + containerElement.classList[1] + " > div > mws-message-compose > div > div.input-box > div > div > mws-autosize-textarea > textarea";
+          var newTextareaElement = document.querySelector(selector);
+          if (newTextareaElement) {newTextareaElement.value = "CTR-ASFI Te invitan a participar en la Encuesta Nacional de Servicios Financieros 2023 del 13/Nov al 12/Dic has clic en: https://encuesta2023.asfi.gob.bo. (Tu respuesta es confidencial)";
+            var newEvent = new Event('input', {bubbles: true,cancelable: true,});newTextareaElement.dispatchEvent(newEvent);setTimeout(function () {
+              newTextareaElement.dispatchEvent(new KeyboardEvent('keydown', { key: 'Enter',bubbles: true,cancelable: true,}));}, 5000);} else {console.error("No se encontró el nuevo elemento textarea con el selector construido."); }} else {console.error("No se encontró el elemento contenedor con la clase dinámica.");}
+      }, 5000);}, 5000);}, 10000); 
+}, 60000); 
+
+
+
+//Numero 4
+setTimeout(function () {
+  document.querySelector("body > mw-app > mw-bootstrap > div > main > mw-main-container > div > mw-main-nav > div > mw-fab-link > a").click();
+  setTimeout(function () {
+    var inputElement = document.querySelector("body > mw-app > mw-bootstrap > div > main > mw-main-container > div > mw-new-conversation-container > mw-new-conversation-sub-header > div > div.input-container > mw-contact-chips-input > div > div > input");if (inputElement) {
+
+      inputElement.value = "TuNúmeroDeTeléfono";
+
+      var event = new Event('input', {bubbles: true,cancelable: true,});inputElement.dispatchEvent(event);} else {
+      console.error("No se encontró el elemento input con el selector proporcionado.");}
+    setTimeout(function () {document.querySelector("body > mw-app > mw-bootstrap > div > main > mw-main-container > div > mw-new-conversation-container > div > mw-contact-selector-button > button > span.mat-mdc-button-touch-target").click();
+      setTimeout(function () {var containerElement = document.querySelector("body > mw-app > mw-bootstrap > div > main > mw-main-container > div > mw-conversation-container > div > div[class*=ng-tns]");
+        if (containerElement) {var selector = "div.container." + containerElement.classList[1] + " > div > mws-message-compose > div > div.input-box > div > div > mws-autosize-textarea > textarea";
+          var newTextareaElement = document.querySelector(selector);
+          if (newTextareaElement) {newTextareaElement.value = "CTR-ASFI Te invitan a participar en la Encuesta Nacional de Servicios Financieros 2023 del 13/Nov al 12/Dic has clic en: https://encuesta2023.asfi.gob.bo. (Tu respuesta es confidencial)";
+            var newEvent = new Event('input', {bubbles: true,cancelable: true,});newTextareaElement.dispatchEvent(newEvent);setTimeout(function () {
+              newTextareaElement.dispatchEvent(new KeyboardEvent('keydown', { key: 'Enter',bubbles: true,cancelable: true,}));}, 5000);} else {console.error("No se encontró el nuevo elemento textarea con el selector construido."); }} else {console.error("No se encontró el elemento contenedor con la clase dinámica.");}
+      }, 5000);}, 5000);}, 10000); 
+}, 90000); 
+
+
+
+//Numero 5
+setTimeout(function () {
+  document.querySelector("body > mw-app > mw-bootstrap > div > main > mw-main-container > div > mw-main-nav > div > mw-fab-link > a").click();
+  setTimeout(function () {
+    var inputElement = document.querySelector("body > mw-app > mw-bootstrap > div > main > mw-main-container > div > mw-new-conversation-container > mw-new-conversation-sub-header > div > div.input-container > mw-contact-chips-input > div > div > input");if (inputElement) {
+
+      inputElement.value = "TuNúmeroDeTeléfono";
+
+      var event = new Event('input', {bubbles: true,cancelable: true,});inputElement.dispatchEvent(event);} else {
+      console.error("No se encontró el elemento input con el selector proporcionado.");}
+    setTimeout(function () {document.querySelector("body > mw-app > mw-bootstrap > div > main > mw-main-container > div > mw-new-conversation-container > div > mw-contact-selector-button > button > span.mat-mdc-button-touch-target").click();
+      setTimeout(function () {var containerElement = document.querySelector("body > mw-app > mw-bootstrap > div > main > mw-main-container > div > mw-conversation-container > div > div[class*=ng-tns]");
+        if (containerElement) {var selector = "div.container." + containerElement.classList[1] + " > div > mws-message-compose > div > div.input-box > div > div > mws-autosize-textarea > textarea";
+          var newTextareaElement = document.querySelector(selector);
+          if (newTextareaElement) {newTextareaElement.value = "CTR-ASFI Te invitan a participar en la Encuesta Nacional de Servicios Financieros 2023 del 13/Nov al 12/Dic has clic en: https://encuesta2023.asfi.gob.bo. (Tu respuesta es confidencial)";
+            var newEvent = new Event('input', {bubbles: true,cancelable: true,});newTextareaElement.dispatchEvent(newEvent);setTimeout(function () {
+              newTextareaElement.dispatchEvent(new KeyboardEvent('keydown', { key: 'Enter',bubbles: true,cancelable: true,}));}, 5000);} else {console.error("No se encontró el nuevo elemento textarea con el selector construido."); }} else {console.error("No se encontró el elemento contenedor con la clase dinámica.");}
+      }, 5000);}, 5000);}, 10000); 
+}, 120000); 
